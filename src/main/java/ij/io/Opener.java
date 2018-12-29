79c5d53f4e0463319e010621596742fadfec2805
Brian Hill <brian@lingerhere.org>
2018-12-29 14:49:16 -0800

    Added .fz as a supported extension.

--- src/main/java/ij/io/Opener.java
+++ src/main/java/ij/io/Opener.java
@@ -1224,7 +1224,7 @@ public class Opener {
 			return ZIP;
 
 		// FITS ("SIMP")
-		if ((b0==83 && b1==73 && b2==77 && b3==80) || name.endsWith(".fts.gz") || name.endsWith(".fits.gz") || name.endsWith(".fit.gz"))
+		if ((b0==83 && b1==73 && b2==77 && b3==80) || name.endsWith(".fts.gz") || name.endsWith(".fits.gz") || name.endsWith(".fit.gz") || name.endsWith(".fz"))
 			return FITS;
 			
 		// Java source file, text file or macro
